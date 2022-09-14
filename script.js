66c6360038351a7da5032679494a87a41dd26544
noman <nomanmd1st2001@gmail.com>
2022-09-15 00:43:44 +0600

    submit fixed & second number clear

--- script.js
+++ script.js
@@ -110,7 +110,7 @@ const start = () => {
     countdownOverlay.innerHTML = `<h1>${count}</h1>`;
 
     // finished timer
-    if (count === 0) {
+    if (count == 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
       countdownOverlay.style.display = "none";
@@ -133,7 +133,7 @@ displayHistory();
 setInterval(() => {
   const currentTime = new Date().getTime();
   const timeSpent = (currentTime - startTime) / 1000;
+  const time = parseInt(timeSpent)
 
-
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+  document.getElementById("show-time").innerHTML = `${startTime ? time : 0} seconds`;
 }, 1000);
